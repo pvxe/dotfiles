b38ec59649c19bab8423c14bc89e9666dc14541b
José María Guisado Gómez <guigom@protonmail.com>
2018-08-15 02:32:58 +0200

    Determine alsa header by pacmd port check
    Using pacmd port check in my only sink I can
    determine if I have my headphone plugged or
    not, so a correspondent font will be shown.

--- awesome/.config/awesome/muhwidgets/muhalsa.lua
+++ awesome/.config/awesome/muhwidgets/muhalsa.lua
@@ -6,7 +6,15 @@ local markup = lain.util.markup
 local myalsa = lain.widget.alsa({
 
     settings = function()
-        header      = volume_now.status == "off" and ' ' or ' '
+        -- Use pacmd to get active port in the only sink I have
+        get_active_port_cmd = "pacmd list-sinks | grep -i 'active port' | cut -d':' -f2"
+        handle      = io.popen(get_active_port_cmd)
+        result      = handle:read("*a")
+        handle:close()
+
+        icon        = string.find(result, "speaker") ~= nil and ' ' or ' '
+        --header      = volume_now.status == "off" and ' ' or ' '
+        header      = icon
         vol_level   = volume_now.level
 
         widget:set_markup(
