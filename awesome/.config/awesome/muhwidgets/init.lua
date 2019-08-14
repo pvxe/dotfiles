9450be8b8475c3dd31146e80ff2faf92c1447d33
José M. Guisado <guigom@protonmail.com>
2019-08-14 15:20:22 +0200

    Remove unused muhbatt_t widget

--- awesome/.config/awesome/muhwidgets/init.lua
+++ awesome/.config/awesome/muhwidgets/init.lua
@@ -1,7 +1,6 @@
 return
 {
     muhbatt = require("muhwidgets.muhbatt");
-    --muhbatt_t = require("muhwidgets.muhbatt_t");
     muhbatt_pb = require("muhwidgets.muhbatt_pb");
     muhalsa = require("muhwidgets.muhalsa");
     muhsettings = require("muhwidgets.muhsettings")
